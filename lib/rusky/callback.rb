3879a9eb46269efd5ce20a7cfd85f0227cf6a520
ohbarye <over.rye@gmail.com>
2017-08-31 00:12:16 +0900

    Add directory check

--- lib/rusky/callback.rb
+++ lib/rusky/callback.rb
@@ -2,11 +2,14 @@ module Rusky
   module Callback
     def self.install
       puts 'installed'
-
+      rusky_dir = File.expand_path(File.dirname($0))
+      puts rusky_dir
     end
 
     def self.uninstall
       puts 'uninstalled'
+      rusky_dir = File.expand_path(File.dirname($0))
+      puts rusky_dir
     end
   end
 end
