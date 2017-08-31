18579fa00dbd1574600a117bc875a908e240b2df
ohbarye <over.rye@gmail.com>
2017-09-01 01:03:27 +0900

    test

--- lib/rusky/callback.rb
+++ lib/rusky/callback.rb
@@ -3,13 +3,19 @@ module Rusky
     def self.install
       puts 'installed'
       rusky_dir = File.expand_path(File.dirname($0))
+      puts 'File.expand_path'
       puts rusky_dir
+      cwd = `lsof -p #{Process.ppid} | grep cwd`.split(" ").last
+      puts cwd
     end
 
     def self.uninstall
       puts 'uninstalled'
       rusky_dir = File.expand_path(File.dirname($0))
+      puts 'File.expand_path'
       puts rusky_dir
+      cwd = `lsof -p #{Process.ppid} | grep cwd`.split(" ").last
+      puts cwd
     end
   end
 end
