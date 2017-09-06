35030abf002735f67bcb5b3e71fcc67f1a2d3e22
ohbarye <over.rye@gmail.com>
2017-09-07 00:17:56 +0900

    Tidy up hook process

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -1,2 +1,10 @@
-require "rubygems/installer"
-require "rusky/hook"
+require "rubygems"
+require "rusky"
+
+Gem.pre_install do |installer|
+  Rusky.install
+end
+
+Gem.pre_uninstall do |uninstaller|
+  Rusky.uninstall
+end
--- lib/rusky/hook.rb
+++
@@ -1,9 +0,0 @@
-require "rusky"
-
-Gem.pre_install do |installer|
-  Rusky.install
-end
-
-Gem.pre_uninstall do |uninstaller|
-  Rusky.uninstall
-end
