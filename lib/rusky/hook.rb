03730005f350bcf78fa0b7aec3ccdf3e9431e517
ohbarye <over.rye@gmail.com>
2018-04-21 22:49:57 +0900

    Add test of Rusky::Hook

--- lib/rusky/hook.rb
+++ lib/rusky/hook.rb
@@ -16,36 +16,38 @@ module Rusky
     end
 
     def create
-      if exists?
+      if !exists?
+        puts "rusky > creating #{hook_name} hook script..."
+        write
+      else
         if is_rusky_hook?
           puts "rusky > overwriting #{hook_name} hook script because also existing one is created by rusky."
           write
         else
           puts "rusky > skip creating #{hook_name} hook script because existing one is created by you or other tool."
         end
-      else
-        puts "rusky > creating #{hook_name} hook script..."
-        write
       end
       self
     end
 
     def delete
-      if is_rusky_hook?
-        puts "rusky > removing #{hook_name} hook script..."
-        File.delete(filename)
+      if !exists?
+        puts "rusky > skip deleting #{hook_name} hook script since there is no git hook file."
+      else
+        if is_rusky_hook?
+          puts "rusky > removing #{hook_name} hook script..."
+          File.delete(filename)
+        else
+          puts "rusky > skip deleting #{hook_name} hook script because existing one is created by you or other tool."
+        end
       end
       self
-    end
-
-    def rake_task_name
-      @rake_task_name ||= "rusky:#{hook_name.gsub('-', '_')}"
     end
 
     def define_task
       # prioritize existing user hook
       if Rake::Task.task_defined? rake_task_name
-        puts "rusky > skip creatig a new rake task since the task for #{hook_name} is already defined."
+        puts "rusky > skip creating a new rake task since the task for #{hook_name} is already defined."
         return
       end
 
@@ -66,8 +68,12 @@ module Rusky
 
     private
 
+    def rake_task_name
+      @rake_task_name ||= "rusky:#{hook_name.gsub('-', '_')}"
+    end
+
     def exists?
-      @exists ||= File.exists? filename
+      @exists ||= File.exists?(filename)
     end
 
     def is_rusky_hook?
@@ -77,6 +83,14 @@ module Rusky
     def write
       File.write filename, hook_script
       FileUtils.chmod(0755, filename)
+    end
+
+    def no_verify_message
+      can_bypass? ? '(add --no-verify to bypass)' : '(cannot be bypassed with --no-verify due to Git specs)'
+    end
+
+    def can_bypass?
+      hook_name != 'prepare-commit-msg'
     end
 
     def hook_script
@@ -103,14 +117,6 @@ module Rusky
         }
       EOS
     end
-  end
-
-  def no_verify_message
-    can_bypass? ? '(add --no-verify to bypass)' : '(cannot be bypassed with --no-verify due to Git specs)'
-  end
-
-  def can_bypass?
-    hook_name != 'prepare-commit-msg'
   end
 end
 
